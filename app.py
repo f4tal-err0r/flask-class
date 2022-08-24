3c04a156686f171bc5b44fd50acca8aaa6f84d91
Ryan Brown <sirloin6002@gmail.com>
2022-08-23 21:24:16 -0400

    got docker webserver up and running with sql

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 
+def get_db_connection():
+    conn = sqlite3.connect('db/blog.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    conn = get_db_connection()
+    posts = conn.execute("SELECT * from posts").fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
 
 if __name__ == "__main__":
     app.run(debug=True)
